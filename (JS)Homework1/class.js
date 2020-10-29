8d09ca7f930cc06c40f1af1cfeedc329f233374b
zhukmarkian <zhukmarkian.t@gmail.com>
2020-10-29 19:53:35 +0200

    qqqq

---
+++ (JS)Homework1/class.js
@@ -0,0 +1,241 @@
+// //task1
+// let str1= 'Hello';
+// let str2='owu';
+// let str3='com';
+// let str4='ua';
+// let num1='1';
+// let num2='10';
+// let num3='-999';
+// let num4='123';
+// let num5='3.14';
+// let num6='2.7';
+// let num7='16';
+// let boolen='true';
+// let boolen2='false';
+// console. log(str1);
+// console. log(str2);
+// console.log(str3);
+// console.log(str4);
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console .log(num4);
+// console. log(num5);
+// console. log(num6);
+// console .log(num7);
+// console .log(boolen);
+// console .log(boolen2);
+// alert(str1);
+// alert(str2);
+// alert(str3);
+// alert(str4);
+// alert(num1);
+// alert(num2);
+// alert(num3);
+// alert(num4);
+// alert(num5);
+// alert(num6);
+// alert(num7);
+// alert(boolen);
+// alert(boolen2);
+// document.write(str1);
+// document.write(str2);
+// document.write(str3);
+// document.write(str4);
+// document.write(num1);
+// document.write(num2);
+// document.write(num3);
+// document.write(num4);
+// document.write(num5);
+// document.write(num6);
+// document.write(num7);
+// document.write(boolen);
+// document.write(boolen2);
+// //task2
+// str1='hye';
+// str2='ovu';
+// str3='cam';
+// str4='uk';
+// num1=2;
+// num2=20;
+// num3=-99;
+// num4=321;
+// num5=3.15;
+// num6=7.2;
+// num7=61;
+// boolen='false';
+// boolen2='true';
+// console. log(str1);
+// console. log(str2);
+// console.log(str3);
+// console.log(str4);
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console .log(num4);
+// console. log(num5);
+// console. log(num6);
+// console .log(num7);
+// console .log(boolen);
+// console .log(boolen2);
+// alert(str1);
+// alert(str2);
+// alert(str3);
+// alert(str4);
+// alert(num1);
+// alert(num2);
+// alert(num3);
+// alert(num4);
+// alert(num5);
+// alert(num6);
+// alert(num7);
+// alert(boolen);
+// alert(boolen2);
+// document.write(str1);
+// document.write(str2);
+// document.write(str3);
+// document.write(str4);
+// document.write(num1);
+// document.write(num2);
+// document.write(num3);
+// document.write(num4);
+// document.write(num5);
+// document.write(num6);
+// document.write(num7);
+// document.write(boolen);
+// document.write(boolen2);
+// //task3
+// const number=1;
+// const number2=2;
+// const number3=3;
+// const string='Привіт' ;
+// const string2='Олег' ;
+// const string3='Як ти?';
+// console.log(number);
+// console.log(number2);
+// console.log(number3);
+// console. log(string);
+// console. log(string2);
+// console.log(string3);
+// alert(number);
+// alert(number2);
+// alert(number3);
+// alert(string);
+// alert(string2);
+// alert(string3);
+// document.write(number);
+// document.write(number2);
+// document.write(number3);
+// document.write(string);
+// document.write(string2);
+// document.write(string3);
+// task4
+// let name =prompt('Як тебе звати?');
+// let secondName =prompt('Яке у тебе прізвище?');
+// let surname =prompt('Як тебе по батькові?');
+// console.log(name);
+// console.log(secondName);
+// console.log(surname);
+// alert(name);
+// alert(secondName);
+// alert(surname);
+// document.write(name);
+// document.write(secondName);
+// document.write(surname);
+// //task5
+//  let name =prompt('Як тебе звати?');
+//  let secondName =prompt('Яке у тебе прізвище?');
+//  let surname =prompt('Як тебе по батькові?')
+// person=name+secondName+surname;
+// console.log(person);
+// alert(person);
+// document.write(person);
+// //task6
+// let name1 =prompt('Як тебе звати?');
+// let secondName1 =prompt('Яке у тебе прізвище?');
+// let surname1 =prompt('Як тебе по батькові?');
+// let name2 =prompt('Як тебе звати?');
+// let secondName2 =prompt('Яке у тебе прізвище?');
+// let surname2 =prompt('Як тебе по батькові?');
+// let name3 =prompt('Як тебе звати?');
+// let secondName3 =prompt('Яке у тебе прізвище?');
+// let surname3 =prompt('Як тебе по батькові?');
+// let nameThree=prompt('Як тебе звати?');
+// let secondNameThree =prompt('Яке у тебе прізвище?');
+// let surnameThree =prompt('Як тебе по батькові?');
+// let name4 =prompt('Як тебе звати?');
+// let secondName4 =prompt('Яке у тебе прізвище?');
+// let surname4 =prompt('Як тебе по батькові?');
+// person1=name1+secondName1+surname1;
+// console.log(person1);
+// person2=name2+secondName2+surname2;
+// console.log(person2);
+// person3=nameThree+secondNameThree+surnameThree;
+// console.log(person3);
+// person4=name4+secondName4+surname4;
+// console.log(person4);
+// //task7
+// let numOne=+prompt('enter number:');
+// let numTwo=+prompt('enter number:');
+// let numThree=+prompt( 'enter number:');
+// let result=numOne+numTwo+numThree;
+// console.log(result);
+// //task8
+// let num1=parseInt(prompt('enter number:'));
+// let num2=parseInt(prompt('enter number:'));
+// let num3=parseInt(prompt( 'enter number:'));
+// let num4=parseInt(prompt( 'enter number:'));
+// let result=num1+num2+num3+num4;
+// console.log(result);
+// //task9
+// let num1=parseFloat(prompt('enter number:'));
+// let num2=parseFloat(prompt('enter number:'));
+// let num3=parseFloat(prompt( 'enter number:'));
+// let result=num1+num2+num3;
+// console.log(result);
+// //task10
+// let num1=parseFloat(prompt('enter number:'));
+// let num2=parseFloat(prompt('enter number:'));
+// let num3=parseFloat(prompt( 'enter number:'));
+// let result=Math.round(num1)+Math.round(num2)+Math.round(num3);
+// console.log(result);
+// //task11
+// let num1=+prompt('enter number:');
+// let num2=+prompt('enter number:');
+// num1=Math.round(num1);
+// num2=Math.round(num2);
+// let result=Math.pow(num1,num2);
+// console.log(result);
+// //task12
+// let a = typeof(100);
+// let b = typeof('100');
+// let c = typeof(true);
+// let d = typeof(undefined);
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+ //task13
+let a = 5 < 6;  //true 
+let b = 5 == 6;  //false
+let c =5 > 6 ;  //false
+let d =5 >= 6 ; //false
+let f = 10 <= 10;  //true
+let g =10 == 10;  //true
+let e =10 > 10; //false;
+let o =10 < 10;  //false;
+let p =10 != 10; //false;
+let s =123 ==='123'; //false ;
+let x =123 == '123'; //true;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(f);
+console.log(g);
+console.log(e);
+console.log(o);
+console.log(p);
+console.log(s);
+console.log(x);
