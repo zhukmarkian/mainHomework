45d179b1c03e2c4fae1d88c888678e7a9e83a8b4
zhukmarkian <zhukmarkian.t@gmail.com>
2020-11-01 11:57:58 +0200

    sss

---
+++ (JS)Homework2/class.js
@@ -0,0 +1,145 @@
+// //task1
+// let arr=[1,2,3,4,5];
+// console.log(arr);
+// //task2
+// let arr=['1','2','3','4','5'];
+// console.log(arr);
+// //task3
+// let arr=[1,2,3,4,5,'1','2','3','4','5',true,false];
+// // console.log(arr);
+// //task4
+// let arr=[];
+//  arr[0]='cat';
+//  arr[1]='dog';
+//  arr[2]='snacke';
+// console.log(arr)
+// //task5
+// let arr=['<div>one</div>','<div>two</div>','<div>three</div>','<div>four</div>','<div>five</div>',
+// '<div>six</div>','<div>seven</div>','<div>eight</div>','<div>nine</div>','<div>ten</div>']
+// for(i=0;i<arr.length;i++){}
+// document.write(arr)
+// //task6
+// let arr=['<div>one</div>','<div>two</div>','<div>three</div>','<div>four</div>','<div>five</div>',
+// '<div>six</div>','<div>seven</div>','<div>eight</div>','<div>nine</div>','<div>ten</div>'];
+// for(i=0;i<arr.length;i++){
+//     document.write(arr[i]);
+//     document.write([i]) ;
+// }
+// //task7
+// let arr=['<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>',
+// '<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>',
+// '<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>'];
+// let i=0;
+// while(i<arr.length){
+//     i++;
+//     document.write(arr);
+// }
+// //task8
+// let arr=['<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>',
+// '<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>',
+// '<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>','<h7>Привіт</h7>'];
+// let i=0
+// while(i<arr.length){
+//     i++;
+//     document.write(arr[i]);
+//     document.write([i]);
+// }
+// //task9
+// let arr=[1,2,3,4,5,6,7,8,9,10];
+// for(i=0;i<arr.length;i++){}
+// // console.log(arr)
+//task10
+// let arr=['1','2','3','4','5','6','7','8','9','10'];
+// for(i=0;i<arr.length;i++){}
+// // console.log(arr)
+//task11
+// let arr=['1','2','3',4,5,6,7,8,true,false];
+// for(i=0;i<arr.length;i++){}
+// console.log(arr)
+// //task12
+// let arr=[1,2,3,4,'5','6','7','8',true,false];
+// for(i=0;i<arr.length;i++){
+//     if(typeof(arr[i])=='boolean'){
+//         console.log(arr[i])
+//     }
+// }
+// //task13
+// let arr=[1,2,3,4,'5','6','7','8',true,false];
+// for(i=0;i<arr.length;i++){
+//     if(typeof(arr[i])=='number'){
+//         console.log(arr[i])
+//     }
+// // }
+// let arr=[1,2,3,4,'5','6','7','8',true,false];
+// for(i=0;i<arr.length;i++){
+//     if(typeof(arr[i])=='string'){
+//         console.log(arr[i])
+//     }
+// }
+// //task14
+// let arr=[];
+// arr[0]='cat';
+// arr[1]=2;
+// arr[2]=3;
+// arr[3]=4;
+// arr[4]='5';
+// arr[5]='dog';
+// arr[6]=true;
+// arr[7]=false;
+// arr[8]=9;
+// arr[9]=10;
+// for(i=0;i<arr.length;i++){
+// }
+// console.log(arr);
+// //task15
+// for(i=0;i<=10;i++){
+// console.log(i)
+// document.write(i)
+// }
+// //task16
+// for(i=0;i<=100;i++){
+//     console.log(i)
+//     document.write(i)
+//     }
+// //task17
+// for(i=0;i<=100;i+=2){
+//     console.log(i)
+//     document.write(i)
+//     }
+// //task18
+// for(i=0;i<=100;i++){
+//     if(i%2==0);
+//     console.log(i);
+//     document.write(i);
+//     }
+//  //task19
+//  for(i=0;i<100;i++){
+//      if(i%2!==0){
+//          console.log(i);
+//          document.write(i)
+//      }
+//     }
+// //task20
+// for(i=0;i<60;i++){
+//     console.log(i+'сек')
+//     if(i==59){
+//         for(x=0;x<59;x++){
+//             console.log('1хв'+x+'сек')
+
+//         }
+//     }
+// }
+// //task21
+// for(i=0;i<2;i++){
+//     console.log(i+'год')
+// if(i==1){
+//         for(x=0;x<19;x++){
+//             console.log('1год'+x+'хв')
+//         }if(x==19){
+//             for(y=0;y<59;y++){
+//                 console.log('1год'+x+'хв'+y+'сек')
+//             }
+//         }
+//     }
+// }
+        
