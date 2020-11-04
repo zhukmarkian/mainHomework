22f07d5f29b58ad030ac9f88693f522ebd2f654b
zhukmarkian <zhukmarkian.t@gmail.com>
2020-11-04 18:16:32 +0200

    dkssdlfsff

---
+++ (JS)Homework3/task1/template.js
@@ -0,0 +1,32 @@
+// //1
+// let elem1=document.getElementById('content');
+// document.write(elem1.textContent);
+// //2
+// let elem2=document.getElementById('rules');
+// document.write(elem2.textContent);
+// //3
+// let elem3=document.getElementById('content');
+// elem3.innerText='Hello World'
+// document.write(elem3.innerText);
+// //task4
+// let elem4=document.getElementById('rules');
+// elem4.innerText='Hello World'
+// document.write(elem3.innerText);
+// //task5
+// let elem5=document.getElementById('content');
+// elem5.style.backgroundColor='red';
+// document.write(elem5);
+// //task6
+// let elem6=document.getElementById('rules');
+// elem6.style.backgroundColor='blue';
+// document.write(elem6)
+// //task7
+// let elem7=document.getElementsByClassName("fc bp");
+// console.log(elem7);
+// //task8
+// let elem8=document.getElementsByClassName("fc_rules");
+// console.log(elem8);
+// //task9 НЕ ПОЛУЧАЄТЬСЯ В ЧОМУ ПРОБЛЕМА?
+// let elem9=document.getElementById("li_class");
+// elem9.style.fontColor='red'
+// document.write(elem9)
---
+++ (JS)Homework3/task2/task2.js
@@ -0,0 +1,525 @@
+// // //task1
+// let dog ={ 
+//   breed:'Bulldog',
+//     name:'Tayson',
+//     color:'black',
+// }
+// let car={
+//     brand:'BMW',
+//     color:'black',
+//     engine:'3.0',
+// }
+// let jacket={
+//  material:'skin',
+//  pockets:4,
+//  brand:'Chanel',
+// }
+// let board={
+//     material:'wood',
+//     producer:'IKEA',
+//     legs:4,
+// }
+// let windows={
+//     rim:'wooden',
+//     glass:'buletproof',
+//     producer:'IKEA'
+//  }
+// //Task2
+// let house={
+//     подвіря:['сад','альтанка','гараж','басейн','мангал'],
+//     розташування:{
+//         місто:'Стрий',
+//         вулиця :'Шумлявщина',
+//         будинок: 15, 
+//     },
+//     колір:'персиковий',
+// сусіди:'приємні в спілкуванні',
+// }
+// let driver={ 
+//  вигляд:[' костюм','сорочка','галстук','окуляри','мешти'],
+//  автомобіль:{
+//      марка: 'Ауді',
+//      колір:'чорний',
+//      мотор:'3.0',
+//      стан:'чудовий',
+//  },
+//  навики:'ідеально',
+//  спілкування:'приємно розмовляти',
+// }
+// let doll={
+// набір:['плаття','гребінь','черевики','аксесуари','сумка'],
+// вигляд:{
+// волосся:'чорне',
+// шкіра:'темна',
+// ріст:'60 см',
+// національність:'азіатка',
+// вік:20,
+// },
+// фірма:'LEGO',
+// дата:'14.01.2009р.'
+// }
+// let table={
+//     матеріал:['скло','дерево','метал','сілікон','резина'],
+//     вигляд:{
+//         дерево:'червоне',
+//         метал:'чугун',
+//         скло:'пуле не пробивне',
+//         ножки:4,
+//         особливість:'вогнестійкий',
+//     },
+//     розмір:'4на2',
+//     виробник:'Меблі стрий'
+// }
+// let bag={
+//     матеріал:['шкіра змії','замок золотий','кишені льяні','ручки тканинні','оправа алмазна'],
+//     Дані:{
+//     фріма:'Chanel',
+//     країна:'Італія',
+//     ціна:'1000$',
+//     колекція:'Animel planet',
+//     доступно:'8 екземплярів'
+//     },
+//     магазин:'Prada',
+//     розташування:'Києв вул.Хрищатик'}
+    // //Task3
+    // for(let key in dog){
+    //     console.log(dog);
+    // }
+    // for(let key in car){
+    //     console.log(car);
+    // }
+    // for(let key in jacket){
+    //     console.log(jacket);
+    // }
+    // for(let key in board){
+    //     console.log(board);
+    // }
+    // for(let key in windows){
+    //     console.log(windows);
+    // }
+    // for(let key in house){
+    //     console.log(house);
+    // }
+    // for(let key in driver){
+    //     console.log(driver);
+    // }
+    // for(let key in doll){
+    //     console.log(doll);
+    // }
+    // for(let key in table){
+    //     console.log(table);
+    // }
+    // for(let key in bag){
+    //     console.log(bag);
+    // }
+//     //task4
+//   console.log(Object.keys(dog));
+//   console.log(Object.keys(car));
+//   console.log(Object.keys(jacket));
+//   console.log(Object.keys(board));
+//   console.log(Object.keys(windows));
+//   console.log(Object.keys(house));
+//   console.log(Object.keys(driver));
+//   console.log(Object.keys(doll));
+//   console.log(Object.keys(table));
+//   console.log(Object.keys(bag));
+// //task5
+// let cars=[{
+//     модель:'BMW' ,
+//     випуск: 2000,
+//     мощність: 3.0,
+//     колір: 'black',
+//     водій: 'Stepan' 
+// },
+// {
+//     модель: 'Mersedes',
+//     випуск: 2020,
+//     мощність: 5.0,
+//     колір: 'red',
+//     водій: 'Andriy',
+// },
+// {
+//     модель: 'Porsche',
+//     випуск: 1960,
+//     мощність: 2.2,
+//     колір: 'blue',
+//     водій: 'Max',
+// },
+// {
+//     модель: 'Mezerati',
+//     випуск: 2010,
+//     мощність: 3.5,
+//     колір: 'brown',
+//     водій: 'Oleg',
+// },
+// {   
+//     модель: 'Ferari',
+//     випуск: 2000,
+//     мощність: 4.5,
+//     колір: 'yellow',
+//     водій: 'Rostuk',
+// },
+// {
+//     модель: 'Skoda',
+//     випуск: 2001,
+//     мощність: 1.6,
+//     колір: 'purple',
+//     водій: 'Mukola',
+// },
+// {
+//     модель: 'Citroen',
+//     випуск: 2005,
+//     мощність: 2.0,
+//     колір: 'pink',
+//     водій: 'Olya',
+// },
+// {
+//     модель:'Audi' ,
+//     випуск: 2019,
+//     мощність: 4.5,
+//     колір: 'green',
+//     водій: 'Maga',
+// },
+// {
+//     модель:'Ford' ,
+//     випуск: 2014,
+//     мощність: 2.2,
+//     колір: 'blueviolet',
+//     водій: 'Oksana',
+// },
+// {
+//     модель: 'Mustang',
+//     випуск: 2015,
+//     мощність: 4.5,
+//     колір: 'orange',
+//     водій: 'Markian',
+// },
+// ]
+// //task6
+// let cities=[{
+//     назва: 'Стрий',
+//     популяція: 70000 ,
+//     страна: 'Україна',
+//     регион: 'Львів', 
+// },
+// {
+//     назва: 'Los Angeles',
+//     популяція: 209089,
+//     страна: 'USA',
+//     регион: 'New York',
+// },
+// {
+//     назва: 'Львів',
+//     популяція: 12000,
+//     страна: 'Україна',
+//     регион: 'Львів',
+// },
+// {
+//     назва: 'Києв',
+//     популяція: 277928,
+//     страна: 'Україна',
+//     регион: 'Києв',
+// }]
+// //task7
+// let car=[{
+//     модель:'BMW' ,
+//     випуск: 2000,
+//     мощність: 3.0,
+//     колір: 'black',
+//     водій:{
+//         імя: 'Max',
+//         вік: 22,
+//         стать: 'men',
+//         стаж: 4,
+//     }
+// }]
+// //task8
+// let i=0;
+// while(i<cars.length){
+//     console.log(cars[i]);
+//     i++;
+// };
+// let a=0;
+// while(a<cities.length){
+// console.log(cities[a]);
+// a++;
+// };
+// let b =0;
+// while(b<car.length){
+//     console.log(car[b]);
+// }
+// //task9
+// let i=0;
+// for(i=0;i<cars.length;i++){
+//     console.log(cars[i]);
+// };
+// let a=0;
+// for(a=0;a<cities.length;a++){
+//     console.log(cities[a]);
+// };
+// let b=0;
+// for(b=0;b<car.length;b++){
+//     console.log(car[b]);
+// };
+// //task10
+// for (let element of cars){
+//     console.log(element)
+// };
+// for (let element of cities){
+//     console.log(element)
+// };
+// for (let element of car){
+//     console.log(element)
+// },
+// //task11
+// let jsonDog=JSON.stringify(dog);
+// let jsonCar=JSON.stringify(car);
+// let jsonJacket=JSON.stringify(jacket);
+// let jsonBoard=JSON.stringify(board);
+// let jsonWindows=JSON.stringify(windows);
+// //task12
+// let d=JSON.parse(jsonDog);
+// let c=JSON.parse(jsonCar);
+// let j=JSON.parse(jsonJacket);
+// let b=JSON.parse(jsonBoard);
+// let w=JSON.parse(jsonwWindows);
+// //task13
+// let jsonCars=JSON.stringify(cars);
+//task14
+// let jsonCities=JSON.stringify(cities);
+// // //task15
+// for (let elements of car){
+//   let jsonC=JSON.stringify(car);
+//   console.log(jsonC)
+// // }
+// //task16
+// let users=[{ name:'oleg',age:20,skills:['python','js']},
+// {name:'andriy',age:23,skills:['mma','javascript']},
+// {name:'max',age:26,skills:['html','css']},];
+// // for(let user of users){
+// //     console.log(user);
+// //     for(let skill of user.skills){
+// //         console.log(skill)
+// //     }
+// // }
+// //task17
+// let newArr=[];
+// for(let user of users){
+//     console.log(user);
+//     for(let skill of user.skills){
+//         newArr.push(skill);
+//     }
+// }
+// console.log(newArr);
+// //task18
+// let users = [{name: 'vasya', age: 31, status: false, skills: ['java', 'js']}, 
+// {name: 'petya', age: 30, status: true, skills: ['java', 'js', 'html']}, 
+// {name: 'kolya', age: 29, status: true, skills: ['mysql', ',mongo']}, 
+// {name: 'olya', age: 28, status: false, skills: ['java', 'js']},
+//  {name: 'max', age: 30, status: true, skills: ['mysql', ',mongo']}];
+// for (let user of users){
+// console.log(user);
+// for(let skill of user.skills){
+    
+// }
+// }
+//task19
+// let users = [{
+//     name: 'vasya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+// }, {
+//     name: 'petya',
+//     age: 30,
+//     status: true,
+//     address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+// }, {
+//     name: 'kolya',
+//     age: 29,
+//     status: true,
+//     address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+// }, {
+//     name: 'olya',
+//     age: 28,
+//     status: false,
+//     address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+// }, {
+//     name: 'max',
+//     age: 30,
+//     status: true,
+//     address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+// }, {
+//     name: 'anya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+// }, {
+//     name: 'oleg',
+//     age: 28,
+//     status: false,
+//     address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+// }, {
+//     name: 'andrey',
+//     age: 29,
+//     status: true,
+//     address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+// }, {
+//     name: 'masha',
+//     age: 30,
+//     status: true,
+//     address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+// }, {
+//     name: 'olya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+// }, {
+//     name: 'max',
+//     age: 31,
+//     status: true,
+//     address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+// }];
+//task19
+// let newArr=[];
+// for(let i of users ){
+//     newArr.push(i.address)
+// }
+// console.log(newArr)
+// //task20
+// for(let i=0;i<users.length;i++){
+//    const user=users[i];
+//    const usersDiv=document.createElement('div');
+//    console.log(user.name, user.age,user.status,user.adress);
+//    usersDiv.innerHTML = user.name + ': ' +user.age + ': '+user.status+': '+user.adress;
+//    document.body.appendChild(usersDiv);
+// }
+// //task21
+// for(let i=0;i<users.length;i++){
+//     const user=users[i];
+//   let divName=document.createElement('div');
+//   let divAge=document.createElement('div');
+//   let divStatus=document.createElement('div');
+//   let divAdress=document.createElement('div');
+//    divName.innerHTML = user.name ;
+//    divAge.innerHTML = user.age ;
+//    divStatus.innerHTML = user.status; 
+//    divAdress.innerHTML = user.address;
+//    document.body.appendChild(divName);
+//    document.body.appendChild(divAge);
+//    document.body.appendChild(divStatus);
+//    document.body.appendChild(divAdress);
+//  }
+// //task22 НЕ РОЗУМІЮ ЧОМУ НЕ ПОЛУЧАЄТЬСЯ?????
+// for(let user of users){
+
+//   let divName=document.createElement('div');
+//   let divAge=document.createElement('div');
+//   let divStatus=document.createElement('div');
+//   let divAdress=document.createElement('div');
+//   let divAdress1=document.createElement('div');
+//   let divAdress2=document.createElement('div');
+//   let divAdress3=document.createElement('div');
+//   let divAdress4=document.createElement('div');
+//    divName.innerHTML = user.name ;
+//    divAge.innerHTML = user.age ; 
+//    divStatus.innerHTML = user.status; 
+//    divAdress1.innerText.innerHTML=user.adress.city;
+//    divAdress2.innerText.innerHTML=user.adress.country;
+//    divAdress3.innerText.innerHTML=user.adress.street;
+//    divAdress4.innerText.innerHTML=user.adress.houseNumber;
+//    document.body.appendChild(divName);
+//    document.body.appendChild(divAge);
+//    document.body.appendChild(divStatus);
+//    document.body.appendChild(divAdress1);
+//    document.body.appendChild(divAdress2);
+//    document.body.appendChild(divAdress3);
+//    document.body.appendChild(divAdress4);
+//  }
+// //TASK23
+// let usersWithId = [{id: 1, name: 'vasya', age: 31, status: false},
+//  {id: 2, name: 'petya', age: 30, status: true}, 
+// {id: 3, name: 'kolya', age: 29, status: true},
+//  {id: 4, name: 'olya', age: 28, status: false},];
+
+
+// let citiesWithId = [{user_id: 3, country: 'USA', city: 'Portland'}, 
+// {user_id: 1, country: 'Ukraine', city: 'Ternopil'}, 
+// {user_id: 2, country: 'Poland', city: 'Krakow'},
+//  {user_id: 4, country: 'USA', city: 'Miami'},];
+//  let usersWithCities=[];
+//  for(let item of usersWithId){
+//      for(cities of citiesWithId){
+//          if(item.id==cities.user_id){
+//              item.adress=cities;
+//          }
+//      }
+//  }
+// usersWithCities.push(usersWithId);
+// console.log(usersWithCities)
+// // //task24
+// let element = document.getElementById('Text');
+// let element1=document.getElementsByClassName('Lorem');
+// let element2=document.getElementsByTagName('p');
+// // element.innerText='Hello';
+// for(let item of element1){
+//     item.innerText='good '
+// }
+// for(let i of element2){
+//     i.innerText='buenos noches'
+// }
+// //task25
+// element.style.width='20px';
+// element.style.height='30px';
+
+// element1.style.width='25px';
+// element1.style.height='40px';
+
+// element2.style.width='30px';
+// element2.style.height='25px';
+// // //task26
+// let table = document.createElement('table');
+// let tr=document.createElement('tr');
+// let td1=document.createElement('td');
+// let td2=document.createElement('td');
+// let td3=document.createElement('td');
+// tr.appendChild(td1);
+// tr.appendChild(td2);
+// tr.appendChild(td3);
+// table.appendChild(tr);
+// document.body.appendChild(table);
+//task27
+// let table = document.createElement('table');
+// for(let i=0;i<10;i++){
+//     let tr=document.createElement('tr');
+//     table.appendChild(tr);
+//     for(let a=0;a<3;a++){
+//         let td =document.createElement('td');
+//         tr.appendChild(td);
+//     }
+// }
+// document.body.appendChild(table);
+// //task28
+// let table = document.createElement('table');
+// for(let i=0;i<;i++){
+//     let tr=document.createElement('tr');
+//     table.appendChild(tr);
+//     for(let a=0;a<5;a++){
+//         let td =document.createElement('td');
+//         tr.appendChild(td);
+//     }
+// }
+// document.body.appendChild(table);
+//task29
+// let table = document.createElement('table');
+// let n=prompt['кількість стовпців'];
+// let m = prompt['кількість рядків'];
+// for(let i=0;i<n;i++){
+//     let tr=document.createElement('tr');
+//     table.appendChild(tr);
+//     for(let a=0;a<m;a++){
+//         let td =document.createElement('td');
+//         tr.appendChild(td);
+//     }
+// }
+// document.body.appendChild(table);
