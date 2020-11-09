dbf1edaae7501dad24062cfe2320bfda490ba3fb
zhukmarkian <zhukmarkian.t@gmail.com>
2020-11-09 20:01:15 +0200

    name commit

---
+++ (JS)Homewrok5/clas.js
@@ -0,0 +1,436 @@
+// //task1
+// let = tagTitle ='a',
+//  let  = tagAction='предназначен для создания ссылок',
+//   let=  atributs=[{
+//         name1:'acceskey',
+//         function1:'Активация силки с помощю клавиш'
+//   }, 
+//   { 
+//       name1:'coords',
+//       function1:'Устанавливает координати активной области.'
+//   }      
+//   ]
+//   function Tag(title,action,arr){
+//       this.title=title;
+//       this.action=action;
+//       this.arr=arr;
+//   }
+//   let tag=new Tag(tagTitle,tagAction,atributs);
+//   console.log(tag)
+
+//  let   tagTitle :'div',
+//     action:'является блочным элементом и предназначен для выделения фрагмента документа с целью изменения вида содержимого',
+//     atributs:[{
+//         classAndId:'Чтобы не описывать каждый раз стиль внутри тега, можно выделить стиль во внешнюю таблицу стилей, а для тега добавить атрибут class или id с именем селектора'
+    
+// }]
+// }
+//  let =tagTitle ='h1',
+// let= tagAction='HTML предлагает шесть заголовков разного уровня, которые показывают относительную важность секции, расположенной после заголовка',
+//   let=  atributs=[{
+//         h1:'Так, тег <h1> представляет собой наиболее важный заголовок первого уровня',
+//         h6:'тег <h6> служит для обозначения заголовка шестого уровня и является наименее значительным.',
+//     }]
+   
+
+// function Tag(title,action,arr){
+//     this.title=title;
+//     this.action=action;
+//     this.arr=arr;
+// }
+
+// let tag=new Tag(tagTitle,tagAction,atributs);
+// console.log(tag)
+// let tagSpan={
+//     title :'span',
+//     action:'предназначен для определения строчных элементов документа',
+//     atributs:[{
+//        atrib: 'Чтобы не описывать каждый раз стиль внутри тега, можно выделить стиль во внешнюю таблицу стилей, а для тега добавить атрибут class или id с именем селектора'
+//     }]
+   
+// }
+
+//   let=  tagTitle ='input',
+//   let=  tagAction='является одним из разносторонних элементов формы и позволяет создавать разные элементы интерфейса и обеспечить взаимодействие с пользователем.',
+//    let= atributs=[{
+//         type:'пределяющий вид элемента',
+//     }]
+   
+//     function Tag(title,action,arr){
+//         this.title=title;
+//         this.action=action;
+//         this.arr=arr;
+//     }
+//     let tag=new Tag(tagTitle,tagAction,atributs);
+//     console.log(tag)
+
+// let= tagTitle='form',
+// let=tagAction='Связывает поле с формой по её идентификатору',
+//  let=  atributs=[{
+//         atribs:'Идентификатор формы (значение атрибута id тега <form>).'
+//     }]
+   
+//     function Tag(title,action,arr){
+//         this.title=title;
+//         this.action=action;
+//         this.arr=arr;
+//     }
+//     let tag=new Tag(tagTitle,tagAction,atributs);
+//     console.log(tag)
+
+//    let= tagTitle ='option',
+//    let= tagAction='определяет отдельные пункты списка, создаваемого с помощью контейнера <select>',
+//    let= atributs=[{
+//         select:' Если планируется отправлять данные списка на сервер, то требуется поместить элемент <select> внутрь формы. Это также необходимо, когда к данным списка идет обращение через скрипты.',
+//     }]
+//     function Tag(title,action,arr){
+//         this.title=title;
+//         this.action=action;
+//         this.arr=arr;
+//     }
+//     let tag=new Tag(tagTitle,tagAction,atributs);
+//     console.log(tag)
+
+// 
+//  let =  tagTitle='select',
+//    let= tagAction=' позволяет создать элемент интерфейса в виде раскрывающегося списка, а также список с одним или множественным выбором, как показано далее',
+//     let=atributs=[{
+//         size:'устанавливает высоту списка',
+//     }]
+
+//     function Tag(title,action,arr){
+//        this.title=title;
+//        this.action=action;
+//       this.arr=arr;
+//    }
+//             let tag=new Tag(tagTitle,tagAction,atributs);
+//             console.log(tag)
+
+//task2
+// class Tag{
+//     constructor(title,action,arr){
+//     this.title=title;
+//     this.action=action;
+//     this.arr=arr;
+//     }
+// }
+// let tag=new Tag(tagTitle,tagAction,atributs);
+// console.log(tag)
+
+// class Tag{
+//     constructor(title,action,arr){
+//     this.title=title;
+//     this.action=action;
+//     this.arr=arr;
+//     }
+// }
+// let tag=new Tag(tagTitle,tagAction,atributs);
+// console.log(tag)
+
+// class Tag{
+//     constructor(title,action,arr){
+//     this.title=title;
+//     this.action=action;
+//     this.arr=arr;
+//     }
+// }
+// let tag=new Tag(tagTitle,tagAction,atributs);
+// console.log(tag)
+
+// class Tag{
+//     constructor(title,action,arr){
+//     this.title=title;
+//     this.action=action;
+//     this.arr=arr;
+//     }
+// }
+// let tag=new Tag(tagTitle,tagAction,atributs);
+// console.log(tag)
+
+// class Tag{
+//     constructor(title,action,arr){
+//     this.title=title;
+//     this.action=action;
+//     this.arr=arr;
+//     }
+// }
+// let tag=new Tag(tagTitle,tagAction,atributs);
+// console.log(tag)
+
+// class Tag{
+//     constructor(title,action,arr){
+//     this.title=title;
+//     this.action=action;
+//     this.arr=arr;
+//     }
+// }
+// let tag=new Tag(tagTitle,tagAction,atributs);
+// console.log(tag)
+
+// class Tag{
+//     constructor(title,action,arr){
+//     this.title=title;
+//     this.action=action;
+//     this.arr=arr;
+//     }
+// }
+// let tag=new Tag(tagTitle,tagAction,atributs);
+// console.log(tag)
+
+// class Tag{
+//     constructor(title,action,arr){
+//     this.title=title;
+//     this.action=action;
+//     this.arr=arr;
+//     }
+// }
+// let tag=new Tag(tagTitle,tagAction,atributs);
+// console.log(tag)
+
+//task3
+// let drivers={name:'Max',age:20};
+// let car={
+//   model:'S8',
+//   producer:'BMW',
+//   year:2019,
+//   MaxSpeed:'260км/год',
+//   engineCapacity:4.0,
+//  drive:function (){
+//       console.log('їдемо зі швидкісю '+this.MaxSpeed )
+//   },
+//   info:function(){
+//       if(this.shafer){
+//     console.log(`
+//         model:${this.model},
+//         producer:${this.producer},
+//         year:${this.year},
+//         MaxSpeed:${this.MaxSpeed},
+//         engineCapacity:${this.engineCapacity},
+//         shafer:${this.shafer.name}
+//      `);}else{
+//          console.log(`    model:${this.model},
+//          producer:${this.producer},
+//          year:${this.year},
+//          MaxSpeed:${this.MaxSpeed},
+//          engineCapacity:${this.engineCapacity},
+//          `);
+//      }
+//   },
+//   increaseMaxSpeed:function(newSpeed){
+//       this.MaxSpeed +=newSpeed;
+//   },
+//   changeYear:function(newValue){
+//       this.year=newValue;
+//   },
+// addDriver:function(driver){
+//    this.shafer=driver;
+// }
+// };
+
+
+
+// car.increaseMaxSpeed('300км/год');
+// car.changeYear(2030);
+// car.addDriver(drivers);
+// car.drive();
+// car.info();
+
+//task4
+// let drivers={name:'Max',age:20};
+// function Car(model,
+//     producer,
+//     year,
+//     MaxSpeed,
+//     engineCapacity){
+//   this.model=model||'S8';
+//   this.producer=producer||'BMW';
+//   this.year=year||2019;
+//   this.MaxSpeed=MaxSpeed||'260км/год';
+//   this.engineCapacity=engineCapacity||4.0;
+//  this.drive=function(){
+//       console.log('їдемо зі швидкісю '+this.MaxSpeed )
+//   },
+//   this.info=function(){
+//       if(this.shafer){
+//     console.log(`
+//         model:${this.model},
+//         producer:${this.producer},
+//         year:${this.year},
+//         MaxSpeed:${this.MaxSpeed},
+//         engineCapacity:${this.engineCapacity},
+//         shafer:${this.shafer.name}
+//      `);}else{
+//          console.log(`    model:${this.model},
+//          producer:${this.producer},
+//          year:${this.year},
+//          MaxSpeed:${this.MaxSpeed},
+//          engineCapacity:${this.engineCapacity},
+//          `);
+//      }
+//   },
+//   this.increaseMaxSpeed=function(newSpeed){
+//       this.MaxSpeed +=newSpeed;
+//   },
+//   this.changeYear=function(newValue){
+//       this.year=newValue;
+//   },
+// this.addDriver=function(driver){
+//    this.shafer=driver;
+// }
+// };
+
+
+// let car=new Car('Lexus',
+// 'China',
+// '2000',
+// 120,
+// 2.0)
+// car.increaseMaxSpeed('300км/год');
+// car.changeYear(2030);
+// car.addDriver(drivers);
+// car.drive();
+// car.info();
+
+//task5
+// let drivers={name:'Max',age:20};
+//  class Car{
+//   constructor(
+//    model,
+//    producer,
+//     year,
+//     MaxSpeed,
+//      engineCapacity,
+//   ){
+//   this.model=model||'s8',
+//   this.producer=producer||'BMW',
+//   this.year=year||2020,
+//   this.MaxSpeed=MaxSpeed||340
+//   this.engineCapacity=engineCapacity||5.0
+//     }
+
+
+//  drive(){
+//       console.log('їдемо зі швидкісю '+this.MaxSpeed );
+//     };
+//   info(){
+//       if(this.shafer){
+//     console.log(`
+//         model:${this.model},
+//         producer:${this.producer},
+//         year:${this.year},
+//         MaxSpeed:${this.MaxSpeed},
+//         engineCapacity:${this.engineCapacity},
+//         shafer:${this.shafer.name}
+//      `);}else{
+//          console.log(`    model:${this.model},
+//          producer:${this.producer},
+//          year:${this.year},
+//          MaxSpeed:${this.MaxSpeed},
+//          engineCapacity:${this.engineCapacity},
+//          `);
+//      }
+//   }
+//   increaseMaxSpeed(newSpeed){
+//       this.MaxSpeed +=newSpeed;
+//   }
+//   changeYear(newValue){
+//       this.year=newValue;
+//   }
+// addDriver(driver){
+//    this.shafer=driver;
+// }
+//  }
+// let car=new Car('Lexus',
+// 'China',
+// '2000',
+// 120,
+// 2.0);
+// car.increaseMaxSpeed('300км/год');
+// car.changeYear(2030);
+// car.addDriver(drivers);
+// car.drive();
+// car.info();
+
+//task6
+
+
+// class Popelushka{
+//     constructor(name,age,footsize){ 
+//         this.name=name;
+//         this.age=age;
+//         this.footsize=footsize;
+//     }
+// }
+// class Princ{
+//     constructor(name,age,shooezsize){
+//         this.name=name;
+//         this.age=age;
+//         this.shooezsize=shooezsize||;
+//     }
+//     findPopelushka(girls){
+//         let find=null;
+//         for(let item of girls){
+//            if (item.footsize===this.shooezsize){
+//                 find=item;
+//             }
+//         }
+//         if(find){
+//             console.log('My Popelushka name'+find.name );
+//         }else{
+//             console.log('I am not find my Popelushka')
+//         }
+//     } 
+// }
+// let girl1=new Popelushka('Karina',18 , 20,) ;
+// let girl2=new Popelushka('Olya',20 , 36,) ;
+// let girl3=new Popelushka('Oksana',23 ,40 ,) ;
+// let girl4=new Popelushka('Ksenia', 24, 41,) ;
+// let girl5=new Popelushka('Katya', 21,30 ,) ;
+// let girl6=new Popelushka('Muroslava',28 ,34 ,) ;
+// let girl7=new Popelushka('Anna', 30, 32,) ;
+// let girl8=new Popelushka('Natalia',18 , 35,) ;
+// let girl9=new Popelushka('Emilia', 21, 34,) ;
+// let girl10=new Popelushka('Marta',23 , 38,) ;
+// let girls=[girl1,girl2,girl3,girl4,girl5,girl6,girl7,girl8,girl9,girl10];
+
+// const princ =new Princ('Max',20,34);
+// princ.findPopelushka(girls);
+
+//task7
+ 'НЕ ПОЛУЧАЄТЬСЯ!!!!'
+// function Popelushka(name,age,footsize){ 
+//         this.name=name;
+//         this.age=age;
+//         this.footsize=footsize;
+// }
+
+// function Princ(name,age,shooezsize){
+//         this.name=name;
+//         this.age=age;
+//         this.shooezsize=shooezsize;
+
+//     this.findPopelushka=function(girls){
+//         let find=null;
+       
+//     find=girls.find(item=>item.footsize===this.footsize)
+//     find
+//     ?console.log('My Popelushka name'+ find.name);
+//     console.log('I am not find my Popelushka')
+//     }
+// }
+
+// let girl1=new Popelushka('Karina',18 , 20,) ;
+// let girl2=new Popelushka('Olya',20 , 36,) ;
+// let girl3=new Popelushka('Oksana',23 ,40 ,) ;
+// let girl4=new Popelushka('Ksenia', 24, 41,) ;
+// let girl5=new Popelushka('Katya', 21,30 ,) ;
+// let girl6=new Popelushka('Muroslava',28 ,34 ,) ;
+// let girl7=new Popelushka('Anna', 30, 32,) ;
+// let girl8=new Popelushka('Natalia',18 , 35,) ;
+// let girl9=new Popelushka('Emilia', 21, 34,) ;
+// let girl10=new Popelushka('Marta',23 , 38,) ;
+// let girls=[girl1,girl2,girl3,girl4,girl5,girl6,girl7,girl8,girl9,girl10];
+
+// const princ =new Princ('Max',20,34);
+// princ.findPopelushka(girls);
+// console.log(princ)
